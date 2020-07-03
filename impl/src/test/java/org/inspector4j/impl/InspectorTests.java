48785cf748d76de3474029d4fa12194e0821b0c0
Delfim Marime <Delfimarime@gmail.com>
2020-07-03 18:38:58 +0200

    PACKAGE RENAME
    Well , this is org.inspector4j afterall

--- impl/src/test/java/org/inspector4j/impl/InspectorTests.java
+++ impl/src/test/java/org/inspector4j/impl/InspectorTests.java
@@ -1,12 +1,15 @@
 package org.inspector4j.impl;
 
+import org.inspector4j.api.Inspector;
+import org.inspector4j.impl.model.Person;
 import org.junit.Test;
 
 public class InspectorTests {
 
     @Test
     public void run() {
-
+        Inspector instance = new InspectorImpl();
+        instance.inspect(new Person());
     }
 
 }
